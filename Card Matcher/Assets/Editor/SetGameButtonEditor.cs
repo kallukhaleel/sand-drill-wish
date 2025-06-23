47cd968ed07f701b52f8956e65d9db9d9896d3a5
nihal shabeer k v <kallukhaleelklz786@gmail.com>
2025-06-23 15:22:24 +0530

    Connect button selection with GameSettings and load new scene based on selected pair number and category

--- Card Matcher/Assets/Editor/SetGameButtonEditor.cs
+++ Card Matcher/Assets/Editor/SetGameButtonEditor.cs
@@ -3,7 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+[CustomEditor(typeof(SetGameButton))]
+[CanEditMultipleObjects]
+[System.Serializable]
+
 public class SetGameButtonEditor : Editor
 {
 
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        SetGameButton myScript = target as SetGameButton;
+
+        switch (myScript.buttonType)
+        {
+            case SetGameButton.EbuttonType.PairNumberBtn:
+                myScript.PairNumber = (GameSettings.EPairNumber) EditorGUILayout.EnumPopup("Pair Number", myScript.PairNumber);
+                break;
+
+            case SetGameButton.EbuttonType.PuzzleCategoryBtn:
+                myScript.PuzzleCategories = (GameSettings.EPuzzleCategories)EditorGUILayout.EnumPopup("Puzzle Category", myScript.PuzzleCategories);
+                break;
+        }
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+
+    }
 }
